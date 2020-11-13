98d75342a730799b6d46239462acbe5ea6878bc9
Haben Mebrahtom <d_haben10@yahoo.com>
2020-11-13 17:47:03 +0100

    first commit

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.use((req, res, next) => {
 });
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/', express.static(path.join(__dirname, 'static')));
 
 app.use(bodyParser.json());
 
