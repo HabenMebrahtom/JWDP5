84d063345a050ac52819e659c0ab913ac25cd247
Haben Mebrahtom <d_haben10@yahoo.com>
2020-12-16 16:19:33 +0100

    add select option and buttion to single item

--- static/item-script.js
+++ static/item-script.js
@@ -16,6 +16,21 @@ async function getItemData() {
                     <h5>${data.name}</h5>
                     <span><i class="fas fa-euro-sign"></i> ${data.price}</span>
                 </div>
+                <div class="d-flex justify-content-center align-items-center my-2">
+                  <select class="form-select form-select-sm form-select-lg px-4 py-2" aria-label=".form-select-sm products-quantity w" >
+                    <option value="1">1</option>
+                    <option value="2">2</option> 
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                  </select>
+                </div>
+                <div class="d-flex justify-content-center align-items-center">
+                   <button href="#" class="btn btn-dark px-4">Buy</button>
+                </div>
             </div>   
     </div>
     `;
